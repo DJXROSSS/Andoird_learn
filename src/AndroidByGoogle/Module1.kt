90c12bac376d670a4e6f02453f8cf9a84666b830
Jatin Kumar <jatinkumar8125@gmail.com>
2024-07-20 14:28:49 +0530

    added the folder for the android class I'm taking directly from the Android website.

--- src/AndroidByGoogle/Module1.kt
+++ src/AndroidByGoogle/Module1.kt
@@ -1,2 +1,41 @@
 package AndroidByGoogle
 
+fun main() {
+    q1()
+    q2()
+    q3()
+}
+
+fun q1(){
+    println("Use the val keyword when the value doesn't change.")
+    println("Use the var keyword when the value can change.")
+    println("When you define a function, you define the parameters that can be passed to it.")
+    println("When you call a function, you pass arguments for the parameters.")
+}
+
+fun q2 () {
+    println("New chat message from a friend")
+}
+
+fun q3() {
+    val discountPercentage = 20
+    val item = "Google Chromecast"
+    val offer = "Sale - Up to $discountPercentage% discount on $item! Hurry up!"
+    println(offer)
+}
+
+fun q4() {
+    val numberOfAdults = 20
+    val numberOfKids = 30
+    val total = numberOfAdults + numberOfKids
+    println("The total party size is: $total")
+    // the original code had the values of numberOfAdults and numbersOfKids as a string, which caused the string concatination to happen
+    // instead of integer addition.
+}
+
+fun q5() {
+    val baseSalary = 5000
+    val bonusAmount = 1000
+    val totalSalary = baseSalary + bonusAmount
+    println("Congratulations for your bonus! You will receive a total of $totalSalary (additional bonus).")
+}
